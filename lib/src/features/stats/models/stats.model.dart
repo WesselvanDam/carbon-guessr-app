ac83e2585dacde64fbe94fc6f2aa193e6aec9d26
WesselvanDam <wesselvandam@hotmail.com>
2026-01-31 12:14:27 +0100

    feat: create stats model and provider

---
+++ lib/src/features/stats/models/stats.model.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'stats.model.freezed.dart';
+part 'stats.model.g.dart';
+
+@freezed
+abstract class StatsModel with _$StatsModel {
+  const factory StatsModel({
+    @Default(0) int gamesFinished,
+    @Default([]) List<double> recentGameScores,
+  }) = _StatsModel;
+  const StatsModel._();
+
+  factory StatsModel.fromJson(Map<String, dynamic> json) =>
+      _$StatsModelFromJson(json);
+
+  double? get averageScore {
+    if (recentGameScores.isEmpty) return null;
+    
+    final total = recentGameScores.reduce((a, b) => a + b);
+    return total / recentGameScores.length;
+  }
+}
---
+++ lib/src/features/stats/providers/stats.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:riverpod_annotation/experimental/persist.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../../client/local_db.dart';
+import '../models/stats.model.dart';
+
+part 'stats.g.dart';
+
+@riverpod
+class Stats extends _$Stats {
+  @override
+  FutureOr<Map<String, StatsModel>> build() async {
+    await persist(
+      ref.watch(localDbClientProvider.future),
+      key: 'stats',
+      options: const StorageOptions(cacheTime: StorageCacheTime.unsafe_forever),
+      encode: jsonEncode,
+      decode: (String encoded) {
+        final decoded = jsonDecode(encoded) as Map<String, dynamic>;
+        return decoded.map(
+          (key, value) =>
+              MapEntry(key, StatsModel.fromJson(value as Map<String, dynamic>)),
+        );
+      },
+    ).future;
+    return state.value ?? {};
+  }
+
+  Future<void> updateStats(String cid, double gameScore) async {
+    final current = state.value ?? {};
+    final stats = current[cid] ?? const StatsModel();
+
+    state = AsyncValue.data({
+      ...current,
+      cid: stats.copyWith(
+        gamesFinished: stats.gamesFinished + 1,
+        recentGameScores: [...stats.recentGameScores, gameScore],
+      ),
+    });
+  }
+}
